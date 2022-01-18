d585f4b5dd34498be0760deb8a029f38aef4b231
Shun Liu <Shun.Liu@noaa.gov>
2022-01-18 00:47:59 +0000

    get_rrfs_retro.sh

--- get_rrfs_retro.sh
+++ get_rrfs_retro.sh
@@ -1,43 +1,43 @@
 
 #this script should be placed in /gpfs/dell6/emc/modeling/noscrub/Shun.Liu/com 
 #get rap observation
-rapdir=/gpfs/hps/nco/ops/com/rap/prod/rap.20211118
-mkdir -p rap/prod/rap.20211118
-cp $rapdir/*bufr* rap/prod/rap.20211118
+mydate=20211208
+rapdir=/gpfs/hps/nco/ops/com/rap/prod/rap.$mydate
+mkdir -p rap/prod/rap.$mydate
+cp $rapdir/*bufr* rap/prod/rap.$mydate
 
-exit
 #get gfs ics
-gfsdir=/gpfs/dell1/nco/ops/com/gfs/prod/gfs.20211118/00/atmos
-mkdir -p gfs/prod/gfs.20211118/00/atmos
-cp $gfsdir/gfs.t00z.atmf006.nc gfs/prod/gfs.20211118/00/atmos
-cp $gfsdir/gfs.t00z.atmf007.nc gfs/prod/gfs.20211118/00/atmos
+gfsdir=/gpfs/dell1/nco/ops/com/gfs/prod/gfs.$mydate/00/atmos
+mkdir -p gfs/prod/gfs.$mydate/00/atmos
+cp $gfsdir/gfs.t00z.atmf006.nc gfs/prod/gfs.$mydate/00/atmos
+cp $gfsdir/gfs.t00z.atmf007.nc gfs/prod/gfs.$mydate/00/atmos
 
 #get gdas ics
-gdasdir=/gpfs/dell1/nco/ops/com/gfs/prod/gdas.20211118/00/atmos
-mkdir -p gfs/prod/gdas.20211118/00/atmos
-cp $gdasdir/gdas.t00z.atmf006.nc gfs/prod/gdas.20211118/00/atmos
-cp $gdasdir/gdas.t00z.atmf007.nc gfs/prod/gdas.20211118/00/atmos
+gdasdir=/gpfs/dell1/nco/ops/com/gfs/prod/gdas.$mydate/00/atmos
+mkdir -p gfs/prod/gdas.$mydate/00/atmos
+cp $gdasdir/gdas.t00z.atmf006.nc gfs/prod/gdas.$mydate/00/atmos
+cp $gdasdir/gdas.t00z.atmf007.nc gfs/prod/gdas.$mydate/00/atmos
 
 #get gfs lbcs
-cp $gfsdir/gfs.t00z.pgrb2.0p25.f00* gfs/prod/gfs.20211118/00/atmos
-cp $gfsdir/gfs.t00z.pgrb2.0p25.f01* gfs/prod/gfs.20211118/00/atmos
-cp $gfsdir/gfs.t00z.pgrb2.0p25.f02* gfs/prod/gfs.20211118/00/atmos
+cp $gfsdir/gfs.t00z.pgrb2.0p25.f00* gfs/prod/gfs.$mydate/00/atmos
+cp $gfsdir/gfs.t00z.pgrb2.0p25.f01* gfs/prod/gfs.$mydate/00/atmos
+cp $gfsdir/gfs.t00z.pgrb2.0p25.f02* gfs/prod/gfs.$mydate/00/atmos
 
 
-memdir=/gpfs/dell1/nco/ops/com/gfs/prod/enkfgdas.20211118/00/atmos
+memdir=/gpfs/dell1/nco/ops/com/gfs/prod/enkfgdas.$mydate/00/atmos
 
 #get enkf mean
-mkdir -p gfs/prod/enkfgdas.20211118/00/atmos
-cp $memdir/gdas.t00z.atmf006.ensmean.nc gfs/prod/enkfgdas.20211118/00/atmos
-cp $memdir/gdas.t00z.sfcf006.ensmean.nc gfs/prod/enkfgdas.20211118/00/atmos
-cp $memdir/gdas.t00z.atmf007.ensmean.nc gfs/prod/enkfgdas.20211118/00/atmos
-cp $memdir/gdas.t00z.sfcf007.ensmean.nc gfs/prod/enkfgdas.20211118/00/atmos
+mkdir -p gfs/prod/enkfgdas.$mydate/00/atmos
+cp $memdir/gdas.t00z.atmf006.ensmean.nc gfs/prod/enkfgdas.$mydate/00/atmos
+cp $memdir/gdas.t00z.sfcf006.ensmean.nc gfs/prod/enkfgdas.$mydate/00/atmos
+cp $memdir/gdas.t00z.atmf007.ensmean.nc gfs/prod/enkfgdas.$mydate/00/atmos
+cp $memdir/gdas.t00z.sfcf007.ensmean.nc gfs/prod/enkfgdas.$mydate/00/atmos
 
 #get enkf mem
 for i in $(seq -f "%03g" 1 80)
 do
   echo $i
-  mkdir -p gfs/prod/enkfgdas.20211118/00/atmos/mem${i}
-  cp $memdir/mem${i}/gdas.t00z.atmf006.nc gfs/prod/enkfgdas.20211118/00/atmos/mem${i}
-  cp $memdir/mem${i}/gdas.t00z.atmf007.nc gfs/prod/enkfgdas.20211118/00/atmos/mem${i}
+  mkdir -p gfs/prod/enkfgdas.$mydate/00/atmos/mem${i}
+  cp $memdir/mem${i}/gdas.t00z.atmf006.nc gfs/prod/enkfgdas.$mydate/00/atmos/mem${i}
+  cp $memdir/mem${i}/gdas.t00z.atmf007.nc gfs/prod/enkfgdas.$mydate/00/atmos/mem${i}
 done
