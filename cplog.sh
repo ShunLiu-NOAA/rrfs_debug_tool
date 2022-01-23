d5cfe4b46e55471e92be8d1f10a9c76cca2ed7e0
Shun Liu <Shun.Liu@noaa.gov>
2022-01-23 15:20:47 +0000

    cplog.sh

---
+++ cplog.sh
@@ -0,0 +1,12 @@
+
+cdate=20220120
+
+logdir=/gpfs/dell2/ptmp/Shun.Liu/fv3lamdax/log
+slogdir=/gpfs/dell2/emc/modeling/noscrub/Shun.Liu/test/lamlog/fv3lamdax_shun/$cdate
+
+
+mkdir -p $slogdir
+
+cp $logdir/regional_forecast_tm*_00* $slogdir
+
+
