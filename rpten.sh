0f5fd11e57d777297ea9ab7270f421596fed5edc
Shun Liu <Shun.Liu@noaa.gov>
2022-01-25 13:45:06 +0000

    rpten.sh

--- rpten.sh
+++ rpten.sh
@@ -5,6 +5,29 @@ echo $SHELL
 echo `date`
 source ~/bin/loadp.sh
 
+
+#cdate=20220122
+#cyc=12
+cdate=$1
+cyc=$2
+
+dataemc=/gpfs/dell2/emc/modeling/noscrub/Shun.Liu/test/lamlog/fv3lamdax_emc/$cdate/$cyc
+datashun=/gpfs/dell2/emc/modeling/noscrub/Shun.Liu/test/lamlog/fv3lamdax_shun/$cdate/$cyc
+datadir=/gpfs/dell1/ptmp/Shun.Liu/tmp
+
+cd $datadir
+grep forecast $dataemc/regional_forecast_tm00_${cyc}.log* > emc
+grep forecast $datashun/regional_forecast_tm00_${cyc}.log* > shun
+
+cd /gpfs/dell2/emc/modeling/save/Shun.Liu/code/rrfs_debug_tool
+ 
+python pltpten.py $datadir/emc \
+                  $datadir/shun \
+                  $cdate$cyc
+mv ptend.png ptend_${cdate}_${cyc}.png
+
+
+exit
 datadir=/gpfs/dell2/emc/modeling/noscrub/Shun.Liu/test/pten
  
 python pltpten.py $datadir/pten_emc_para \
