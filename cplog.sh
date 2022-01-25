1b41a362367fa50bb014706d8bc60cd95b121039
Shun Liu <Shun.Liu@noaa.gov>
2022-01-25 13:44:22 +0000

    cplog.sh

--- cplog.sh
+++ cplog.sh
@@ -1,12 +1,34 @@
+#!/bin/bash
 
-cdate=20220120
+mydate=20220122
+cyc=12
+cdate=$mydate$cyc
+
+#logdir=/gpfs/dell2/emc/modeling/noscrub/Shun.Liu/test/lamlog/fv3lamdax_shun/20220119
 
 logdir=/gpfs/dell2/ptmp/Shun.Liu/fv3lamdax/log
-slogdir=/gpfs/dell2/emc/modeling/noscrub/Shun.Liu/test/lamlog/fv3lamdax_shun/$cdate
+slogdir=/gpfs/dell2/emc/modeling/noscrub/Shun.Liu/test/lamlog/fv3lamdax_shun/$mydate/$cyc
 
+#logdir=/gpfs/dell2/ptmp/emc.campara/fv3lamdax/log
+#slogdir=/gpfs/dell2/emc/modeling/noscrub/Shun.Liu/test/lamlog/fv3lamdax_emc/$mydate/$cyc
 
 mkdir -p $slogdir
 
-cp $logdir/regional_forecast_tm*_00* $slogdir
+cp -p $logdir/regional_forecast_tm*_${cyc}* $slogdir
 
+cd $slogdir
 
+files=`ls -1tr regional*`
+for ifile in $files
+do
+  echo $ifile
+  tdate=`grep CDATE $ifile | cut -c7-17`
+  thisdate=${tdate:0:10}
+  echo $thisdate
+
+  if [ $thisdate =  $cdate ]; then
+     echo $thisdate
+  else
+     rm -f $ifile
+  fi
+done
