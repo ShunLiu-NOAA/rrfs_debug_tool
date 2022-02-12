1355d0cd7f4a5521c25de46a18402e0b7d4c33ee
Shun Liu <Shun.Liu@noaa.gov>
2022-02-12 04:05:27 +0000

    cpfit.sh

---
+++ cpfit.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+
+for ((i=5;i>=3;i--));
+do
+  echo $i
+
+mydate=`date "+%Y%m%d%H" --date="$i hour ago"`
+echo $mydate
+
+rundir=/mnt/lfs4/BMC/nrtrr/NCO_dirs/stmp/tmpnwprd/RRFS_NA_3km_dev1/$mydate
+
+fitdir=/mnt/lfs1/BMC/wrfruc/Shun.Liu/test/jet_fit2obs/$mydate
+
+if [ -d $rundir/anal_gsi_spinup ]
+then
+mkdir -p $fitdir/anal_gsi_spinup
+cp $rundir/anal_gsi_spinup/fort.2* $fitdir/anal_gsi_spinup
+cp $rundir/anal_gsi_spinup/stdout $fitdir/anal_gsi_spinup
+fi
+
+if [ -d $rundir/anal_gsi ]
+then
+mkdir -p $fitdir/anal_gsi
+cp $rundir/anal_gsi/fort.2* $fitdir/anal_gsi
+cp $rundir/anal_gsi/stdout $fitdir/anal_gsi
+fi
+
+
+done
