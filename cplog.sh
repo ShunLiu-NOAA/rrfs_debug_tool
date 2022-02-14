7d0d201ae5e6cffc2ca8150d26129180faaed96d
Shun Liu <Shun.Liu@noaa.gov>
2022-02-14 13:16:02 +0000

    cplog.sh

--- cplog.sh
+++ cplog.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-mydate=20220124
-cyc=00
+#mydate=20220201
+#cyc=00
+mydate=$1
+cyc=$2
 cdate=$mydate$cyc
 
 #logdir=/gpfs/dell2/emc/modeling/noscrub/Shun.Liu/test/lamlog/fv3lamdax_shun/20220119
 
-#logdir=/gpfs/dell2/ptmp/Shun.Liu/fv3lamdax/log
-#slogdir=/gpfs/dell2/emc/modeling/noscrub/Shun.Liu/test/lamlog/fv3lamdax_shun/$mydate/$cyc
+logdir=/gpfs/dell2/ptmp/Shun.Liu/fv3lamdax/log
+slogdir=/gpfs/dell2/emc/modeling/noscrub/Shun.Liu/test/lamlog/fv3lamdax_shun/$mydate/$cyc
 
-logdir=/gpfs/dell2/ptmp/emc.campara/fv3lamdax/log
-slogdir=/gpfs/dell2/emc/modeling/noscrub/Shun.Liu/test/lamlog/fv3lamdax_emc/$mydate/$cyc
+#logdir=/gpfs/dell2/ptmp/emc.campara/fv3lamdax/log
+#slogdir=/gpfs/dell2/emc/modeling/noscrub/Shun.Liu/test/lamlog/fv3lamdax_emc/$mydate/$cyc
 
 mkdir -p $slogdir
 
