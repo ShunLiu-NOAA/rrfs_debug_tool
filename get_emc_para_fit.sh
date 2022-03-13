b25db42ab998fca0c3a099ea363dc01b4ea49a8b
Shun Liu <Shun.Liu@noaa.gov>
2022-03-13 03:22:16 +0000

    get_emc_para_fit.sh

--- get_emc_para_fit.sh
+++ get_emc_para_fit.sh
@@ -3,30 +3,32 @@
 
 USER=Shun.Liu
 #sdate=2022011900
-PDY=20220217
+PDY=20220310
 cyc=12
 
 sdate=${PDY}$cyc
 
 echo 'into getfit.sh'
 
-fitdir=/gpfs/dell2/ptmp/Shun.Liu/fv3lamdax/fv3lamdax.$PDY/$cyc
+#fitdir=/gpfs/dell2/ptmp/Shun.Liu/fv3lamdax/fv3lamdax.$PDY/$cyc
 
-if [ -d $fitdir ]; then
-rdasNA=/gpfs/dell2/emc/modeling/noscrub/Shun.Liu/test/fitobs/fv3lamdax_shun.$PDY/$cyc
-mkdir -p $rdasNA
-cp $fitdir/*fits.* $rdasNA 
-cd $rdasNA
-tm='tm06 tm05 tm04 tm03 tm02 tm01 tm00'
-for itm in $tm
-do
-  sed -e 's/asm 900     /asm 900 0000/' fv3lam.t${cyc}z.fits.$itm > $itm
-  mv $itm fv3lam.t${cyc}z.fits.$itm
-done
-fi
+#if [ -d $fitdir ]; then
+#rdasNA=/gpfs/dell2/emc/modeling/noscrub/Shun.Liu/test/fitobs/fv3lamdax_shun.$PDY/$cyc
+#mkdir -p $rdasNA
+#cp $fitdir/*fits.* $rdasNA 
+#cd $rdasNA
+#tm='tm06 tm05 tm04 tm03 tm02 tm01 tm00'
+#for itm in $tm
+#do
+#  sed -e 's/asm 900     /asm 900 0000/' fv3lam.t${cyc}z.fits.$itm > $itm
+#  mv $itm fv3lam.t${cyc}z.fits.$itm
+#done
+#fi
 
-fitdirp=/gpfs/dell2/ptmp/emc.campara/fv3lamdax/fv3lamdax.$PDY/$cyc
-rdasNAt=/gpfs/dell2/emc/modeling/noscrub/Shun.Liu/test/fitobs/fv3lamdax_emc.$PDY/$cyc
+#fitdirp=/gpfs/dell2/ptmp/emc.campara/fv3lamdax/fv3lamdax.$PDY/$cyc
+#rdasNAt=/gpfs/dell2/emc/modeling/noscrub/Shun.Liu/test/fitobs/fv3lamdax_emc.$PDY/$cyc
+fitdirp=/gpfs/dell5/ptmp/emc.campara/fv3lamda/fv3lamda.$PDY/$cyc
+rdasNAt=/gpfs/dell2/emc/modeling/noscrub/Shun.Liu/test/fitobs/fv3lamda_emc.$PDY/$cyc
 mkdir -p $rdasNAt
 cp $fitdirp/*fits.* $rdasNAt 
 cd $rdasNAt
