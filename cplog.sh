dab7b1ae8ec7ae6a9ed07d368da13b2ba07960ee
Shun Liu <Shun.Liu@noaa.gov>
2022-03-25 03:22:27 +0000

    cplog.sh

--- cplog.sh
+++ cplog.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
-#mydate=20220201
-#cyc=00
-mydate=$1
-cyc=$2
+for ((i=120;i>=6;i--));
+do
+  echo $i
+
+mydate=`date "+%Y%m%d" --date="$i hour ago"`
+cyc=`date "+%H" --date="$i hour ago"`
+#mydate=20220324
+cyc00=00
+cyc12=12
+#mydate=$1
+#cyc=$2
 cdate=$mydate$cyc
 
-#logdir=/gpfs/dell2/emc/modeling/noscrub/Shun.Liu/test/lamlog/fv3lamdax_shun/20220119
+if [[ "$cyc" == "00" || "$cyc" == "12" ]]; then
 
-logdir=/gpfs/dell2/ptmp/Shun.Liu/fv3lamdax/log
-slogdir=/gpfs/dell2/emc/modeling/noscrub/Shun.Liu/test/lamlog/fv3lamdax_shun/$mydate/$cyc
+#logdir=/gpfs/dell2/ptmp/Shun.Liu/fv3lamdax/log
+#slogdir=/gpfs/dell2/emc/modeling/noscrub/Shun.Liu/test/lamlog/fv3lamdax_shun/$mydate/$cyc
 
-#logdir=/gpfs/dell2/ptmp/emc.campara/fv3lamdax/log
-#slogdir=/gpfs/dell2/emc/modeling/noscrub/Shun.Liu/test/lamlog/fv3lamdax_emc/$mydate/$cyc
+logdir=/gpfs/dell2/ptmp/emc.campara/fv3lamdax/log
+slogdir=/gpfs/dell2/emc/modeling/noscrub/Shun.Liu/test/lamlog/fv3lamdax_emc/$mydate/$cyc
 
 mkdir -p $slogdir
 
@@ -33,4 +40,32 @@ do
   else
      rm -f $ifile
   fi
+done
+
+logdir=/gpfs/dell5/ptmp/emc.campara/fv3lamda/log
+slogdir=/gpfs/dell2/emc/modeling/noscrub/Shun.Liu/test/lamlog/fv3lamda_emc/$mydate/$cyc
+
+mkdir -p $slogdir
+
+cp -p $logdir/regional_forecast_tm*_${cyc}* $slogdir
+
+cd $slogdir
+
+files=`ls -1tr regional*`
+for ifile in $files
+do
+  echo $ifile
+  tdate=`grep CDATE $ifile | cut -c7-17`
+  thisdate=${tdate:0:10}
+  echo $thisdate
+
+  if [ $thisdate =  $cdate ]; then
+     echo $thisdate
+  else
+     rm -f $ifile
+  fi
+done
+
+fi
+
 done
