19966e249215ca166c9194317fb262d6054afa3a
Shun Liu <Shun.Liu@noaa.gov>
2022-04-15 13:59:23 +0000

    cron_cplog_rrfs.sh

--- cron_cplog_rrfs.sh
+++ cron_cplog_rrfs.sh
@@ -4,7 +4,7 @@ mydate=`date "+%Y%m%d" --date="24 hour ago"`
 cyc=`date "+%H" --date="24 hour ago"`
 echo $mydate
 
-logdir=/gpfs/dell1/ptmp/emc.campara/ptmp/com/logs/RRFS_CONUS/RRFS_conus_3km.$mydate/$cyc
+logdir=/gpfs/dell1/ptmp/emc.campara/logs/rrfs_a/rrfs_a.$mydate/$cyc
 savedir=/gpfs/dell2/emc/modeling/noscrub/Shun.Liu/test/lamlog/rrfs.$mydate/$cyc
 
 if [ -d $rundir ]
