d18357c6d907888bc65abef2e6088a7411aed1f6
Shun Liu <Shun.Liu@noaa.gov>
2022-04-15 13:59:07 +0000

    new

--- cplog_rrfs.sh
+++ cplog_rrfs.sh
@@ -7,7 +7,7 @@ mydate=`date "+%Y%m%d" --date="$i hour ago"`
 cyc=`date "+%H" --date="$i hour ago"`
 echo $mydate
 
-logdir=/gpfs/dell1/ptmp/emc.campara/ptmp/com/logs/RRFS_CONUS/RRFS_conus_3km.$mydate/$cyc
+logdir=/gpfs/dell1/ptmp/emc.campara/logs/rrfs_a/rrfs_a.$mydate/$cyc
 savedir=/gpfs/dell2/emc/modeling/noscrub/Shun.Liu/test/lamlog/rrfs.$mydate/$cyc
 
 if [ -d $logdir ]
@@ -17,7 +17,7 @@ then
   cp $logdir/run_fcst_spinup_*.log $savedir
 fi
 
-logdir=/gpfs/dell1/ptmp/Shun.Liu/ptmp/com/logs/RRFS_CONUS/RRFS_conus_3km.$mydate/$cyc
+logdir=/gpfs/dell1/ptmp/Shun.Liu/logs/rrfs_a/rrfs_a.$mydate/$cyc
 savedir=/gpfs/dell2/emc/modeling/noscrub/Shun.Liu/test/lamlog/rrfsx.$mydate/$cyc
 
 if [ -d $logdir ]
