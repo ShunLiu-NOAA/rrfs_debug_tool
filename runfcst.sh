336e21a69f7209890918974b9ac9b35efd347778
Shun Liu <Shun.Liu@noaa.gov>
2023-07-02 04:03:18 +0000

    runfcst.sh

--- runfcst.sh
+++ runfcst.sh
@@ -1,111 +1,68 @@
-#!/bin/sh
-#BSUB -P RRFS-T2O
-#BSUB -eo test.err
-#BSUB -oo test.out
-#BSUB -J fv3.c768
-#BSUB -W 00:10
-#BSUB -q "dev2"
-#BSUB -n 896
-#BSUB -R span[ptile=20]
-#BSUB -R affinity[core(2):distribute=balance]
-#
+#! /bin/sh
+#PBS -A RRFS-DEV
+#PBS -q pridev
+#PBS -l select=13:mpiprocs=60:ompthreads=2:ncpus=120:mem=500G
+#PBS -l walltime=00:40:00
+#PBS -N test_mem0001
+#PBS -j oe -o test.log
+#PBS -l place=excl
 set -x
 
-#. /usrx/local/prod/lmod/lmod/init/sh
+module purge
+
+module load envvar/1.0
+
+module load PrgEnv-intel/8.1.0
+module load intel/19.1.3.304
+module load craype/2.7.13
+module load cray-mpich/8.1.7
+
+export HPC_OPT=/apps/ops/para/libs
+module use /apps/ops/para/libs/modulefiles/compiler/intel/19.1.3.304
+module use /apps/ops/para/libs/modulefiles/mpi/intel/19.1.3.304/cray-mpich/8.1.7
+
+module load jasper/2.0.25
+module load zlib/1.2.11
+module load libpng/1.6.37
+
+module load hdf5/1.10.6
+module load netcdf/4.7.4
+module load pio/2.5.2
+module load esmf/8.3.0b09
+module load fms/2022.01
+
+module load libjpeg/9c
+module load cray-pals/1.1.3
+module load udunits/2.2.28
+module load gsl/2.7
+module load nco/4.9.7
+
+    ulimit -s unlimited
+    ulimit -a
+    export OMP_PROC_BIND=true
+    export OMP_NUM_THREADS=2
+    export OMP_STACKSIZE=1G
+    export OMP_PLACES=cores
+    export MPICH_ABORT_ON_ERROR=1
+    export MALLOC_MMAP_MAX_=0
+    export MALLOC_TRIM_THRESHOLD_=134217728
+    export FORT_FMT_NO_WRAP_MARGIN=true
+    export MPICH_REDUCE_NO_SMP=1
+    export FOR_DISABLE_KMP_MALLOC=TRUE
+    export FI_OFI_RXM_RX_SIZE=40000
+    export FI_OFI_RXM_TX_SIZE=40000
+    export MPICH_OFI_STARTUP_CONNECT=1
+    export MPICH_OFI_VERBOSE=1
+    export MPICH_OFI_NIC_VERBOSE=1
+
+FV3_EXE=/lfs/h2/emc/lam/noscrub/emc.lam/rrfs/v0.4.1/ufs-srweather-app/bin/ufs_model
 
 
-export HOMEfv3=/gpfs/dell6/emc/modeling/noscrub/Shun.Liu/fv3lamda/regional_workflow
-export PARMfv3=$HOMEfv3/parm
-export EXECfv3=$HOMEfv3/exec
-export USHfv3=$HOMEfv3/ush
-export UTILfv3=$HOMEfv3/util/ush
-export LD_LIBRARY_PATH="/gpfs/dell6/emc/modeling/noscrub/Eric.Rogers/fv3lam_for_dellp3.5/sorc/regional_forecast.fd/ccpp/lib/${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
-
-#set -x -u -e
-set -x
-#export HOMEfv3='/gpfs/dell6/emc/modeling/noscrub/Shun.Liu/fv3lamda/regional_workflow'
-#export job='regional_forecast_tm06'
-#export machine='wcoss_dell_p3.5'
-
-date
-
-. ${HOMEfv3}/rocoto/machine-setup.sh
-export machine=wcoss_dell_p3.5
-
-if [ "$machine" = "wcoss_dell_p3" ] ; then
-  . /usrx/local/prod/lmod/lmod/init/sh
-elif [ "$machine" = "wcoss_dell_p3.5" ] ; then
-  . /usrx/local/prod/lmod/lmod/init/sh
-elif [ "$machine" = "wcoss_cray" ] ; then
-  . /opt/modules/default/init/sh
-elif [ "$machine" = "hera" ] ; then
-  . /apps/lmod/lmod/init/sh
-elif [ "$machine" = "jet" ] ; then
-  . /apps/lmod/lmod/init/sh
-fi
-
-jobpre=regional_forecast_tm06
-module use ${HOMEfv3}/modulefiles/wcoss_dell_p3.5
-jobpre=$(echo ${job} | cut -c1-17)
-if [ "${jobpre}" = "regional_forecast" ]; then
-  module load fv3
-elif [ "${jobpre}" = "regional_bufrpost" ]; then
-  module load bufr
-elif [ "${jobpre}" = "regional_post_con" ]; then
-  module load post
-elif [ "${jobpre}" = "regional_firstgue" ]; then
-  module load chgres_cube
-elif [ "${jobpre}" = "regional_make_bc_" ]; then
-  module load chgres_cube
-elif [ "${jobpre}" = "regional_postgoes" ]; then
-  module load post
-else
-  module load regional
-fi
-
-set -x
-
-echo Shun test
-module list
-export OMP_THREADS='2'
-export TOTAL_TASKS='896'
-export TASK_X='28'
-export TASK_Y='28'
-export WG='2'
-export WTPG='56'
-export NCTSK='20'
-
-export KMP_STACKSIZE=1024m
-export KMP_AFFINITY=disabled
-
-export KMP_AFFINITY=scatter   
-export OMP_STACKSIZE=2048m  
-
-ulimit -s unlimited
-ulimit -a
+#cd /lfs/h2/emc/stmp/emc.lam/rrfs/v0.4.1/2023032411/fcst_fv3lam
+cd /lfs/h2/emc/stmp/emc.lam/rrfs/v0.4.1/2023032806/mem0001/fcst_fv3lam_spinup_one
+mpiexec -n 780 -ppn 64 --cpu-bind core --depth 2 $FV3_EXE
 
 
-#module load impi/18.0.1
-#module load lsf/10.1
-module list
-module use /gpfs/dell2/emc/modeling/noscrub/emc.nemspara/soft/modulefiles
-module list
-module load esmf/8.1.0bs21
 
-export OMP_NUM_THREADS=4
+#/lfs/h2/emc/lam/noscrub/emc.lam/rrfs/v0.4.1/ufs-srweather-app/regional_workflow/ush/load_modules_run_task.sh "run_fcst" "/lfs/h2/emc/lam/noscrub/emc.lam/rrfs/v0.4.1/ufs-srweather-app/regional_workflow/jobs/JREGIONAL_RUN_FCST"
 
-#exec "$@"
-
-#cd /gpfs/dell5/ptmp/Shun.Liu/fv3lamda/tmpnwprd/regional_forecast_tm06_2021081112
-#EXEC=/gpfs/dell6/emc/modeling/noscrub/Shun.Liu/fv3lamda/regional_workflow/exec/regional_forecast.x
-#mpirun $EXEC > outtest 2> errtest
-
-#----------------------------------------- 
-# Run the forecast
-#-----------------------------------------
-
-EXECfv3=/gpfs/dell6/emc/modeling/noscrub/Shun.Liu/fv3lamda/regional_workflow/exec
-export pgm=regional_forecast.x
-#. prep_step
-#startmsg
-mpirun $EXECfv3/regional_forecast.x >pgmout 2>err
