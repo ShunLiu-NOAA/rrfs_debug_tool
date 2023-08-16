8716020345a046e90621ad34d1e9f9421566607a
Shun Liu <Shun.Liu@noaa.gov>
2023-08-16 14:45:25 +0000

    runana.sh

--- runana.sh
+++ runana.sh
@@ -10,7 +10,7 @@
 
 #/lfs/h2/emc/da/noscrub/Shun.Liu/rrfs/testD/ufs-srweather-app/regional_workflow/ush/load_modules_run_task.sh run_anal_gsi /lfs/h2/emc/da/noscrub/Shun.Liu/rrfs/testD/ufs-srweather-app/regional_workflow/jobs/JREGIONAL_RUN_ANAL
 
-source /lfs/h2/emc/lam/noscrub/emc.lam/rrfs/v0.5.3/ufs-srweather-app/env/build_wcoss2_intel.env
+source /lfs/h2/emc/lam/noscrub/emc.lam/rrfs/v0.6.3/ufs-srweather-app/env/build_wcoss2_intel.env
 
 
 ulimit -s unlimited
@@ -19,7 +19,8 @@ ulimit -a
 set -x -u -e
 date
 
-rundir=/lfs/h2/emc/ptmp/emc.lam/Shun.Liu/anal_conv_gsi_spinup
+#rundir=/lfs/h2/emc/ptmp/emc.lam/Shun.Liu/anal_conv_gsi_spinup
+rundir=/lfs/h2/emc/ptmp/emc.lam/Shun.Liu/anal_conv_dbz_gsi_spinup_all
 
 cd $rundir
 
@@ -27,9 +28,9 @@ rm -f pe0*
 rm -f obs_input*
 rm -f stdout
 
-cp ./bk/* .
+#cp ./bk/* .
 
-EXEC=/lfs/h2/emc/lam/noscrub/emc.lam/rrfs/v0.5.3/ufs-srweather-app/bin/gsi.x
+EXEC=/lfs/h2/emc/lam/noscrub/emc.lam/rrfs/v0.6.3/ufs-srweather-app/bin/gsi.x
 
 APRUN="mpiexec -n 480 -ppn 8 --cpu-bind core --depth 16"
 export FI_OFI_RXM_SAR_LIMIT=3145728
@@ -39,6 +40,7 @@ export OMP_NUM_THREADS=16
 #export OMP_PLACES=threads
 
 $APRUN $EXEC <gsiparm.anl > stdout 2> stderr
+exit
 
 cd /lfs/h2/emc/lam/save/emc.lam/Shun.Liu/code/rrfs_debug_tool
 expdir=/lfs/h2/emc/lam/noscrub/emc.lam/Shun.Liu/test/gsi/exp8
