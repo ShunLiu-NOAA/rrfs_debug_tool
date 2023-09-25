56de09f86e0914fb4ed6f787b9fef7481697eef7
Shun Liu <Shun.Liu@noaa.gov>
2023-09-25 17:37:04 +0000

    getfit.sh

--- rrfs_plt_obs/getfit.sh
+++ rrfs_plt_obs/getfit.sh
@@ -11,14 +11,14 @@ echo 'into getfit.sh'
 
 rdasNA=/lfs/h2/emc/lam/save/emc.lam/Shun.Liu/code/rrfs_debug_tool/rrfs_plt_obs/fitobs/na/$PDY
 mkdir -p $rdasNA/$sdate
-rm -f fit_na
+rm -f fit_conus
 
 allcyc="00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23"
 for cyc in $allcyc
 do
-fitdir=/lfs/f2/t2o/ptmp/emc/ptmp/emc.lam/rrfs/v0.5.3/prod/rrfs.$PDY/$cyc
+fitdir=/lfs/f2/t2o/ptmp/emc/ptmp/emc.lam/rrfs/v0.5.3/prod//rrfs.$PDY/$cyc
 cp $fitdir/*fits.* $rdasNA
 cd $rdasNA
-echo $PDY$cyc >> fit_na
-grep "asm 900" rrfs_a.t${cyc}z.fits.tm00 | grep "o-g 01" >> fit_na
+echo $PDY$cyc >> fit_conus
+grep "asm 900" rrfs_a.t${cyc}z.fits.tm00 | grep "o-g 01" >> fit_conus
 done
