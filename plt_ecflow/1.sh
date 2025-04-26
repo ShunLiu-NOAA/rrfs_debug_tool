4e7828b19137977b2584bf5fd99db9f8bfed1b62
Shun Liu <Shun.Liu@noaa.gov>
2025-04-26 03:55:32 +0000

    plt_ecflow/

---
+++ plt_ecflow/1.sh
@@ -0,0 +1,6 @@
+
+#grep LOG submit.ec | grep nco_rrfs_dev_14 | grep jrrfs_enkf_forecast_mem | grep "16.4.2025"
+#grep LOG complete.ec | grep nco_rrfs_dev_14 | grep jrrfs_enkf_forecast_mem | grep "16.4.2025"
+
+grep LOG submit.ec | grep nco_rrfs_dev_15 | grep "16.4.2025"
+grep LOG complete.ec | grep nco_rrfs_dev_15 | grep "16.4.2025"
