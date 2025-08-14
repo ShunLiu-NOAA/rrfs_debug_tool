7bcaf65e41444089672a2293c1c2965caa9946f3
Shun Liu <Shun.Liu@noaa.gov>
2025-08-14 04:16:00 +0000

    get_hpss.sh

--- gfs_plt_grib/get_hpss.sh
+++ gfs_plt_grib/get_hpss.sh
@@ -1,21 +1,26 @@
+##!/bin/bash
+
 #PBS -A RRFS-DEV
 #PBS -q dev_transfer
-#PBS -l select=1:mpiprocs=1:ncpus=1
-#PBS -l walltime=05:55:00
-#PBS -N get_retro
-#PBS -j oe -o get_retro.log
+#PBS -l select=1:ncpus=1:mem=2G
+#PBS -l walltime=5:59:00
+#PBS -j oe
+#PBS -N log_get_gfs_hpss
+
 set -x
 
 mkdir -p /lfs/h2/emc/ptmp/emc.lam/Shun.Liu
 cd /lfs/h2/emc/ptmp/emc.lam/Shun.Liu
 
-#for ihh in $(seq -f "%03g" 0 12)
-#for ihh in {12..21..3}
+#YEAR=${version},MON="03",sdate="15",edate="31"
 
-mon=12
-year=2024
-for iday in $(seq -f "%02g" 1 31)
+mon=$MON
+year=$YEAR
+sdate=$SDATE
+edate=$EDATE
+for iday in $(seq -f "%02g" $sdate $edate)
 do
-htar xf /NCEPPROD/hpssprod/runhistory/rh${year}/${year}${mon}/${year}${mon}${iday}/com_gfs_v16.3_gfs.${year}${mon}${iday}_00.gfs_pgrb2.tar ./gfs.${year}${mon}${iday}/00/atmos/gfs.t00z.pgrb2.0p25.f000
+ echo $iday
+ htar xf /NCEPPROD/hpssprod/runhistory/rh${year}/${year}${mon}/${year}${mon}${iday}/com_gfs_v16.3_gfs.${year}${mon}${iday}_00.gfs_pgrb2.tar ./gfs.${year}${mon}${iday}/00/atmos/gfs.t00z.pgrb2.0p25.f000
 done
 
